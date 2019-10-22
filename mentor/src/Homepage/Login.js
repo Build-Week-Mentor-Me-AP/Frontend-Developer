ee51f30df856551c9cf162bd08509b5d01b7254b
Eric Foster <elitheroyalone@gmail.comgit config --global user.name eric-fostergit config --global user.email elitheroyalone@gmail.com>
2019-10-22 13:26:04 -0500

    Login Form almost Done

--- mentor/src/Homepage/Login.js
+++ mentor/src/Homepage/Login.js
@@ -1,20 +1,42 @@
 mport React, {useState} from "react";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
+import axios from "axios"
 
 
 const LoginForm = ({ values, touched, errors, status }) => {
-    const [user, setUser] = useState([])
+    const [user, setUser] = useState([]);
+    useEffect(() => {
+      if (status) {
+          setUsers([...users, status]);
+      }
+  }, [status])
 
 
 <div> //styles component
     <Form>
         <Field type="text" name="email" placeholder="email"/>
-        <ErrorMessage name="email" />
+        {touched.email && errors.email && (
+        <p className = 'error'>{errors.email}</p>)}
         <Field type="text" name="password" placeholder="password"/>
-        <ErrorMessage name="password" />
+        {touched.password && errors.password && (
+        <p className = 'error'>{errors.password}</p>)}
+        <label className="checkbox-container">
+          Vaccinations
+          <Field
+            type="checkbox"
+            name="save"
+            checked={values.save}
+          />
+          <span className="checkmark" />
         <button type="submit">Submit</button>
     </Form>
+    {users.map(user => (
+                <ul key={user.id}>
+                    <li>Email: {user.email}</li>
+                    <li>Password: {user.password}</li>
+                    </ul>
+            ))}
 </div>
 
 
@@ -23,6 +45,7 @@ const FormikLoginForm = withFormik({
       return {
         email: values.email || "",
         password: values.password || "",
+        save: values.save || false
       };
     },
     validationSchema: Yup.object().shape({
@@ -36,7 +59,7 @@ const FormikLoginForm = withFormik({
         .max(16)
         .matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*d)[a-zA-Zd]$")
         .required(),
-    })
+    }),
 
     handleSubmit(values, { setStatus }) {
         axios
